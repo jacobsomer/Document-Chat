f71c3ea6788c813075f80c5d67742ff1ebc04285
Jacob Somer <somerjacob@gmail.com>
2023-04-27 01:38:19 -0400

    fixed modal button

--- src/components/drawer/addMedia.tsx
+++ src/components/drawer/addMedia.tsx
@@ -195,12 +195,28 @@ const AddMedia = (props: AddMediaProps) => {
       )}
 
       <input type="checkbox" id="my-modal-2" className="modal-toggle" />
-      <label htmlFor="my-modal-2" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
+        <div className="modal">
+          <div className="modal-box relative">
+
           {isMobile ? (
-            <h3 className="font-base-content text-3xl">Press To Add Data</h3>
+            <>
+             <label
+              htmlFor="my-modal-2"
+              className="btn-circle btn-lg btn absolute right-2 top-2 text-3xl"
+            >
+              ✕
+            </label>
+            
+            <h3 className="font-base-content text-3xl">Press To Add Data</h3></>
           ) : (
-            <h3 className="font-base-content text-lg">Add Data</h3>
+            <>
+             <label
+              htmlFor="my-modal-2"
+              className="btn-sm btn-circle btn absolute right-2 top-2"
+            >
+              ✕
+            </label>
+            <h3 className="font-base-content text-lg">Add Data</h3></>
           )}
 
           <UploadSquare handleFileUpload={handleFileUpload} />
@@ -232,10 +248,14 @@ const AddMedia = (props: AddMediaProps) => {
                       Submit
                     </button>
                   </div>
+                    <p className='text-xl'>
+                    Supported URLs include, youtube videos, wikipedia articles, news, and more.
+                  </p>
                 </>
               ) : (
                 <>
-                  <p>Enter URL:</p>
+                  <p>Enter URL </p>
+                
                   <div className="flex gap-x-4">
                     <input
                       placeholder="ex. https://www.youtube.com/watch?v=qbIk7-JPB2c"
@@ -254,7 +274,11 @@ const AddMedia = (props: AddMediaProps) => {
                     >
                       Submit
                     </button>
+                    
                   </div>
+                    <p className='text-sm'>
+                    Supported URLs include, youtube videos, wikipedia articles, news, and more.
+                  </p>
                 </>
               )}
 
@@ -275,8 +299,8 @@ const AddMedia = (props: AddMediaProps) => {
             </form>
           </div>
           <div className="modal-action"></div>
-        </label>
-      </label>
+        </div>
+      </div>
     </>
   );
 };
