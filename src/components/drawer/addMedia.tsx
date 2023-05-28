68a755b00e9ff0a2888c6f2cdbd3aa46a1f85b9d
Jacob Somer <somerjacob@gmail.com>
2023-05-27 23:24:49 -0400

    added file

--- src/components/drawer/addMedia.tsx
+++ src/components/drawer/addMedia.tsx
@@ -1,14 +1,13 @@
 import { useReducer, useState } from 'react';
 import UploadSquare from './uploadSquare';
-import { supportedExtensions, unstructuredExtensions } from '~/utils/consts';
+import { supportedExtensions } from '~/utils/consts';
 
 import { FiUpload } from 'react-icons/fi';
-import { File } from '~/types/types';
+import { File as MediaFile } from '~/types/types';
 import { handleObjectUpload } from '~/utils/handleUpload';
 import { v4 } from 'uuid';
 import { type AddMediaProps } from '~/types/types';
 import { isMobile } from 'react-device-detect';
-import { useRouter } from 'next/router';
 import { createClient } from '@supabase/supabase-js';
 import FileMetadata from '../sidebar/fileMetadata';
 
@@ -22,14 +21,14 @@ const AddMedia = (props: AddMediaProps) => {
   const [errorMessage, setErrorMessage] = useState('');
   const [input, setInput] = useState('');
   const [loadingForAWhile, setLoadingForAWhile] = useState(false);
-  const router = useRouter();
-
-  const origin =
+ 
+const origin =
     typeof window !== 'undefined' && window.location.origin
       ? window.location.origin
       : '';
+  const isLocal = origin.includes('localhost');
   // Create a single supabase client for interacting with your database
-  const supabase = origin.includes('localhost')
+  const supabase = isLocal
     ? createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL_DEV || '',
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_DEV || ''
@@ -46,14 +45,49 @@ const AddMedia = (props: AddMediaProps) => {
     }, 4000);
   };
 
+  const uploadFile = async (
+    file: File,
+    name: string,
+    extension: string
+  ): Promise<string> => {
+    // upload file to supabase storage
+    const { data, error } = await supabase.storage
+      .from('media')
+      .upload(`userFiles/${props.chatId}/${name}.${extension}`, file, {
+        cacheControl: '3600',
+        upsert: true
+      });
+    if (error && !error.message.includes('The resource already exists')) {
+      console.log(error.message);
+      return 'Error';
+    }
+    let url = '';
+    if (data) {
+      url = data.path;
+    } else {
+      url = `userFiles/${props.chatId}/${name}.${extension}`;
+    }
+    const baseStorageUrl =
+      (isLocal
+        ? 'https://eyoguhfgkfmjnjpcwblg.supabase.co'
+        : 'https://gsaywynqkowtwhnyrehr.supabase.co') +
+      '/storage/v1/object/public/media/';
+    url = baseStorageUrl + url;
+    return url;
+  };
+
   const fileUpload = async (
     event: React.ChangeEvent<HTMLInputElement>,
-    successCallback: () => Promise<void>, errorCallback: () => Promise<void>,
+     successCallback: () => void, errorCallback: () => void,
   ) => {
+    setLoading(true);
+    setLoadingForAWhile(false);
+    setTimeout(() => {
+      setLoadingForAWhile(true);
+    }, 10000);
     const file = event.target.files?.[0];
 
     if (file) {
-      // Step 1: parse the file name and extension
       const extension = file.name.split('.').pop();
 
       if (!extension || !supportedExtensions.includes(extension)) {
@@ -61,124 +95,108 @@ const AddMedia = (props: AddMediaProps) => {
           'FileType is not one of: ' + supportedExtensions.toString()
         );
         removeErrorMessageAfter4Seconds();
-        const closeModal = document.getElementById('closeModal');
-        if (closeModal) {
-          closeModal.click();
-        }
+        errorCallback();
         return;
       }
 
       // get file name
       const name = file.name.split('.').slice(0, -1).join('.');
+
       const cleaned_name = cleanFileName(name);
 
-      // Step 2: call the right logic per file extension type. Either we have a 
-      // relatively normal/unstructured/NLP extension supported by JS, or send it to the backend to handle. 
-      if (
-        extension === 'pdf' ||
-        extension === 'txt' ||
-        extension === 'docx' ||
-        extension === 'csv' ||
-        unstructuredExtensions.includes(extension)
-      ) {
-        const formData = new FormData();
-        formData.append('file', file);
-        const response = await fetch(
-          '/api/upload/handleFileUpload' +
-            '?chatId=' +
-            props.chatId +
-            '&name=' +
-            cleaned_name +
-            '&extension=' +
-            extension,
-          {
-            method: 'POST',
-            body: formData
-          }
-        );
-        const resp = (await response.json()) as { message: string };
-        if (resp.message === 'File uploaded successfully') {
-          await props.updateFiles(props.chatId);
-          void supabase.storage
-            .from('media')
-            .upload(
-              `userFiles/${props.chatId}/${cleaned_name}.${extension}`,
-              file,
-              {
-                cacheControl: '3600',
-                upsert: false
-              }
-            );
-            await successCallback();
-        } else {
-          console.log(resp.message);
-          await errorCallback();
-        }
-      } else {
-        const { data, error } = await supabase.storage
-          .from('media')
-          .upload(
-            `userFiles/${props.chatId}/${cleaned_name}.${extension}`,
-            file,
-            {
-              cacheControl: '3600',
-              upsert: false
-            }
-          );
-        if (error && !error.message.includes('The resource already exists')) {
-          setErrorMessage(error.message);
-          removeErrorMessageAfter4Seconds();
-          return;
-        }
-        let url = '';
-        if (data) {
-          url = data.path;
-        } else {
-          url = `userFiles/${props.chatId}/${cleaned_name}.${extension}`;
-        }
-        const baseStorageUrl =
-          'https://gsaywynqkowtwhnyrehr.supabase.co/storage/v1/object/public/media/';
-        url = baseStorageUrl + url;
-        const newDocId = v4();
-        const apiURL =
-          '/api/upload/getEmbeddingsForText?chatId=' +
-          props.chatId +
-          '&name=' +
-          cleaned_name +
-          '&url=' +
-          url +
-          '&newDocId=' +
-          newDocId;
-        const response = await fetch(apiURL, {
+      const url = await uploadFile(file, cleaned_name, extension);
+      if (url === 'Error') {
+        setErrorMessage('Error uploading file');
+        removeErrorMessageAfter4Seconds();
+        errorCallback();
+        return;
+      }
+
+      const enpointURL = `/api/upload/handleFileUpload`;
+      let resp = null;
+      try {
+        const res = await fetch(enpointURL, {
           method: 'POST',
+          body: JSON.stringify({
+            url: url,
+            chatId: props.chatId,
+            name: cleaned_name,
+            extension: extension
+          }),
           headers: {
             'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ url: url })
+          }
         });
-        if (!response.ok) {
-          await errorCallback();
-          setErrorMessage('Error with API');
-          removeErrorMessageAfter4Seconds();
+
+        resp = (await res.json()) as { message: string };
+        if (resp.message === 'File uploaded successfully') {
+          void props.updateFiles(props.chatId);
+          const closeModal = document.getElementById('closeModal');
+          if (closeModal) {
+            closeModal.click();
+          }
+          successCallback();
           setLoading(false);
           setLoadingForAWhile(false);
-        } else {
-          await successCallback();
+          return;
         }
-        const resp = (await response.json()) as { message: string };
-        if (resp.message === 'File uploaded successfully') {
-          await props.updateFiles(props.chatId);
-        }
+      } catch (e) {
+        errorCallback();
+        setErrorMessage('Internal server error');
+        removeErrorMessageAfter4Seconds();
+        setLoading(false);
+        setLoadingForAWhile(false);
+        return;
       }
-      await successCallback();
+    }
+  };
+
+  const handleUrlUpload = async (
+    event: React.MouseEvent<HTMLButtonElement>,
+     successCallback: () => Promise<void>, errorCallback: () => Promise<void>,
+  ): Promise<void> => {
+    event.preventDefault();
+    setLoading(true);
+    setLoadingForAWhile(false);
+    setTimeout(() => {
+      setLoadingForAWhile(true);
+    }, 10000);
+    const enpointURL = '/api/upload/handleUrlUpload';
+    const response = await fetch(enpointURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url: input, chatId: props.chatId })
+    });
+    if (!response.ok) {
+      await errorCallback();
+      setErrorMessage('Error with API');
+      removeErrorMessageAfter4Seconds();
+      setLoading(false);
+      setLoadingForAWhile(false);
       return;
     }
-  }
+    const resp = (await response.json()) as { message: string };
+    if (resp.message === 'File uploaded successfully') {
+      await props.updateFiles(props.chatId);
+      const closeModal = document.getElementById('closeModal');
+      if (closeModal) {
+        closeModal.click();
+      }
+      await successCallback();
+      setLoading(false);
+      setLoadingForAWhile(false);
+      return;
+    }
+    else{
+       await errorCallback();
+    }
+  };
 
   const handleFileUpload = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-
     setLoading(true);
     setLoadingForAWhile(false);
     setTimeout(() => {
@@ -194,11 +212,11 @@ const AddMedia = (props: AddMediaProps) => {
       docId: "DocID placeholder", // TODO
       docName: event.target.files?.[0] ? event.target.files?.[0].name : "upload error",
     });
-    const successCallback = async () => { 
+    const successCallback = () => { 
       console.log("upload successful, metadata object mutated")
       metadata.finishLoading(); 
     }; // TODO
-    const errorCallback = async () => {
+    const errorCallback = () => {
       console.log("upload unsuccessful, metadata object mutated")
       metadata.finishLoading(); 
     }; // TODO
@@ -207,67 +225,6 @@ const AddMedia = (props: AddMediaProps) => {
     await fileUpload(event, successCallback, errorCallback);
     setLoading(false);
     setLoadingForAWhile(false);
-    return;
-  };
-
-  const handleUrlUpload = async (
-    event: React.MouseEvent<HTMLButtonElement>
-  ): Promise<void> => {
-    event.preventDefault();
-    setLoading(true);
-    setLoadingForAWhile(false);
-    setTimeout(() => {
-      setLoadingForAWhile(true);
-    }, 10000);
-    // check if url is already in userdocuments
-    const { data: userDocs, error: userDocsError } = await supabase
-      .from('userdocuments')
-      .select('*')
-      .eq('url', input);
-    if (userDocsError) {
-      setErrorMessage(userDocsError.message);
-      removeErrorMessageAfter4Seconds();
-    }
-    if (userDocs && userDocs.length > 0) {
-      if (userDocs[0]) {
-        const docId = userDocs[0].docId as string;
-        const { error: insertError } = await supabase.from('chats').insert({
-          chatId: props.chatId,
-          docId: docId
-        });
-        if (insertError) {
-          setErrorMessage(insertError.message);
-          removeErrorMessageAfter4Seconds();
-          return;
-        }
-        await props.updateFiles(props.chatId);
-        setLoading(false);
-        return;
-      }
-    }
-    const newDocId = v4();
-    const { docId: docId, error: error1 } = await handleObjectUpload(
-      input,
-      newDocId
-    );
-    if (error1) {
-      setErrorMessage(error1);
-      removeErrorMessageAfter4Seconds();
-      return;
-    }
-    const { error: insertError } = await supabase.from('chats').insert({
-      chatId: props.chatId,
-      docId: docId
-    });
-    if (insertError) {
-      setErrorMessage(insertError.message);
-      removeErrorMessageAfter4Seconds();
-      return;
-    }
-    await props.updateFiles(props.chatId);
-    setLoading(false);
-    setLoadingForAWhile(false);
-
     return;
   };
 
